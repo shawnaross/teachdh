223ceff3663e121594af9d9ecbcefa11af4f2d4f
oncomouse <oncomouse@gmail.com>
2020-05-27 14:14:16 -0500

    Working

--- assets/js/data.js
+++
@@ -1,3 +0,0 @@
----
----
-window.teachDHData = {{site.posts | jsonify}}
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,26 +1,23 @@
 $(function () {
-  function filterQuestions(results) {
-    if (results.length === 0) {
-      $('article').each(function (_i, article) {
-        $(article).show();
-      });
-    } else {
-      var ids = R.pluck('id', results);
-      $('article').each(function (_i, article) {
-        ids.indexOf($(article).attr('id')) >= 0 ? $(article).show() : $(article).hide()
-      });
-    }
+  // Utility functions:
+  function pluck(key, list) {
+    return $.map(list, function (item) {return item[key];});
   }
-  var fuseData = [];
-  $('article').each(function (_i, article) {
+
+  function search(fuse, target) {
+    return pluck('item', fuse.search(target));
+  }
+
+  // Data structures:
+  var fuseData = $('article').map(function (_i, article) {
     var $article = $(article);
-    fuseData.push({
+    return {
       id: $article.attr('id'),
       title: $article.find('h1').text(),
       content: $article.find('main').text(),
-      categories: $article.find('header li').map(function (_i, li) {return $(li).text();}),
-    });
-  });
+      categories: $article.find('header li').map(function (_i, li) {return $(li).text();}).get(),
+    };
+  }).get();
   var fuse = {
     questions: new Fuse(fuseData, {
       keys: [
@@ -34,11 +31,35 @@ $(function () {
       ],
     })
   };
-  var $searchBar = $('#searchBar');
-  var results = [];
-  $searchBar.on('change', function (ev) {
-    ev.preventDefault();
-    results = fuse.questions.search(ev.target.value);
-    filterQuestions(results);
+  // Filter event handler:
+  $('#questions').on('filter', function (_ev, extra) {
+    var input = extra.input;
+    var results = extra.results;
+    if (results.length === 0 && input.length === 0) {
+      $('article').show();
+    } else {
+      var ids = pluck('id', results);
+      $('article').each(function (_i, article) {
+        ids.indexOf($(article).attr('id')) >= 0 ? $(article).show() : $(article).hide()
+      });
+    }
   });
-})
+  // Filters as the user types in the search bar:
+  $('#searchBar').on('input', function (ev) {
+    ev.preventDefault();
+    var value = ev.target.value;
+    $('#questions').trigger('filter', {
+      results: search(fuse.questions, value),
+      input: value,
+    });
+  });
+  // Filters as the user clicks category buttons:
+  $('#categories button').on('click', function (ev) {
+    ev.preventDefault();
+    var value = $(ev.target).attr('data-target');
+    $('#questions').trigger('filter', {
+      results: search(fuse.categories, value),
+      input: value,
+    });
+  });
+});
