816f0c7cc2a235d19f5b9adf2fa05d72226faacb
oncomouse <oncomouse@gmail.com>
2020-05-29 09:51:46 -0500

    publish: Changed to default Pygments theme for highlighting.
    generated from commit 25de30c7cdcc3c147fa6117794a428540abc488a

--- assets/js/list.js
+++
@@ -1,1 +0,0 @@
-function pluck(key,list){return list.map(function(item){return item[key]})}function uniq(list){return list.filter(function(item,index){return list.indexOf(item)===index})}function flatten(list,d){var d=d||Infinity;return d>0?list.reduce(function(acc,val){return acc.concat(Array.isArray(val)?flatten(val,d-1):val)},[]):list.slice()}
