b1af86cf258da40c1bde511d019d73e72fd47dcb
oncomouse <oncomouse@gmail.com>
2020-07-14 18:47:23 -0500

    Better build script

--- _scripts/embed-js.js
+++ _scripts/embed-js.js
@@ -1,36 +1,52 @@
 const fs = require('fs');
-const path = require('path');
-const {JSDOM} = require('jsdom');
 const yaml = require('js-yaml');
-const _argv = process.argv.slice(2)
-// Use --delete to delete files after process them
-// Use --inline to replace file inclusions with file source.
-// If --inline is not used, all CSS and JS assets are packed into a single file
-// which is included.
-const argv = {
-  delete: _argv.indexOf('--delete') >= 0,
-  inline: _argv.indexOf('--inline') >= 0,
-  _: _argv.filter(x => x.indexOf('--') !== 0),
-}
-
-const config = yaml.safeLoad(fs.readFileSync(__dirname + '/../_config.yml'));
-config.baseurl = config.baseurl || '';
-config.url = config.url || '';
-
+const path = require('path');
+const {execSync} = require('child_process');
+const {JSDOM} = require('jsdom');
+const argv = require('minimist')(process.argv.slice(2), {
+  boolean: [
+    'inline',
+    'delete',
+  ],
+  default: {
+    'inline': false,
+    'delete': false,
+  },
+});
+const disallowedList = [
+  'imagesloaded',
+];
+const jekyllConfig = yaml.safeLoad(fs.readFileSync('./_config.yml'))
+const checkDisallowedList = src => disallowedList.reduce((acc, cur) => acc || src.indexOf(cur) >= 0, false);
+// Use with --inline to replace inclusions with their file source
+const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
+const gitRevision = execSync('git rev-parse HEAD').toString().replace(/\n/g, '');
 const processInclude = (el, type, files) => {
   const src = el.getAttribute(type === 'css' ? 'href' : 'src');
   if (src.indexOf('polyfill.io') >= 0) return;
+  if (src.indexOf('googleapis') >= 0) return;
   if (src.indexOf('cdnjs.cloudflare.com') >= 0) return;
   if (src.indexOf('unpkg.com') >= 0) return;
   if (src.indexOf('rawgit.com') >= 0) return;
   if (src.indexOf('jsdelivr.net') >= 0) return;
+  if (checkDisallowedList(src)) {
+    el.parentNode.removeChild(el);
+    return;
+  }
   if (!files.has(src)) {
-    const fileName = path.join(...src.replace(`${config.url}${config.baseurl}/`, './_site/').split('/'));
-    const fileSrc = fs.readFileSync(fileName).toString();
-    files.addFile(src, fileSrc);
-    if (argv.delete) {
-      fs.unlinkSync(fileName);
+    let filePath = src
+    if (has(jekyllConfig, 'url') && jekyllConfig.url.length > 0) {
+      filePath = filePath.replace(jekyllConfig.url, '')
     }
+    if (has(jekyllConfig, 'baseurl') && jekyllConfig.baseurl.length > 0) {
+      filePath = filePath.replace(jekyllConfig.baseurl, '')
+    }
+    filePath = path.join('_site', filePath)
+    const fileSrc = fs.readFileSync(filePath).toString();
+    if (fs.existsSync(filePath) && argv.delete) {
+      fs.unlinkSync(filePath)
+    }
+    files.addFile(src, fileSrc);
   }
   if (argv.inline) {
     // Write the file as an inline inclusion:
@@ -67,7 +83,7 @@ argv._.forEach((file) => {
   const html = fs.readFileSync(file);
   const dom = (new JSDOM(html))
   const document = dom.window.document;
-  const seenCss = Array.from(document.querySelectorAll('link[href]')).reduce((_seen, el) => {
+  const seenCss = Array.from(document.querySelectorAll('link[rel="stylesheet"]')).reduce((seen, el) => {
     processInclude(el, 'css', files);
     return true;
   }, false);
@@ -75,15 +91,16 @@ argv._.forEach((file) => {
     processInclude(el, 'js', files);
     return true;
   }, false);
+  const absoluteUrl = file => `${has(jekyllConfig, 'url') ? jekyllConfig.url : ''}${has(jekyllConfig, 'baseurl') ? jekyllConfig.baseurl : ''}/${file}`
   if (!argv.inline) {
     // If the site had CSS assets, we write the packed CSS file:
     if (seenCss) {
-      const cssPath = `${config.url}${config.baseurl}/packed.css`;
+      const cssPath = absoluteUrl(`packed-${gitRevision}.css`);
       document.querySelector('head').insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="${cssPath}" />`);
     }
     // If the site had JS assets, we write the packed JS file:
     if (seenJs) {
-      const jsPath = `${config.url}${config.baseurl}/packed.js`;
+      const jsPath = absoluteUrl(`packed-${gitRevision}.js`);
       document.querySelector('body').insertAdjacentHTML('beforeend', `<script src="${jsPath}" />`);
     }
   }
@@ -92,12 +109,12 @@ argv._.forEach((file) => {
 if (!argv.inline) {
   // Write the packed CSS file:
   fs.writeFileSync(
-    path.join('_site', 'packed.css'),
+    path.join('_site', `packed-${gitRevision}.css`),
     files.getFiles(/\.css$/).join(''),
   );
   // Write the packed JS file:
   fs.writeFileSync(
-    path.join('_site', 'packed.js'),
+    path.join('_site', `packed-${gitRevision}.js`),
     files.getFiles(/\.js$/).join(''),
   );
 }
