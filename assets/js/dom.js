fb1ed2cc15b3ad1563157639f5288872bbfd6b59
oncomouse <oncomouse@gmail.com>
2020-07-14 10:30:25 -0500

    Polyfill classList

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -2,29 +2,13 @@ function nl2array(nodeList) {
   return Array.prototype.slice.call(nodeList);
 }
 
-var hasClass = (typeof document.documentElement.classList == 'undefined') ?
-  function (el, clss) {
-    return el.className && new RegExp("(^|\\s)" +
-      clss + "(\\s|$)").test(el.className);
-  } :
-  function (el, clss) {
-    return el.classList.contains(clss);
-  };
+function hasClass(el, clss) {
+  return el.className && new RegExp("(^|\\s)" +
+    clss + "(\\s|$)").test(el.className);
+}
 
 function toggleClass(el, clss) {
-  if (typeof document.documentElement.classList == 'undefined') {
-    el.classList.toggle(clss);
-  } else {
-    // For IE9
-    var classes = el.className.split(" ");
-    var i = classes.indexOf(clss);
-
-    if (i >= 0)
-      classes.splice(i, 1);
-    else
-      classes.push(clss);
-    el.className = classes.join(" ");
-  }
+  el.classList.toggle(clss);
 }
 
 function show(element) {
