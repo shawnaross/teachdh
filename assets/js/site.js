1f90c62bff2031271e2d9e5249f3dd5d3d4ee14b
oncomouse <oncomouse@gmail.com>
2020-07-14 14:53:14 -0500

    Initial Commit

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,9 @@
-/* globals pluck, nl2array, uniq, flatten, Fuse, show, hide, hasClass, toggleClass */
+/* globals Fuse, Stickyfill, pluck, nl2array, uniq, flatten, show, hide, hasClass, toggleClass */
 document.addEventListener('DOMContentLoaded', function () {
-  var ACTIVE_CLASS = 'active';
+  Stickyfill.add(document.querySelector('#controls'));
+  var ACTIVE_CLASS = 'bg-moon-gray';
+  var INACTIVE_CLASS = 'bg-white';
+  var BUTTON_CLASS = 'f6 grow no-underline br-pill ba ph3 pv2 mb2 mr2 black bg-white b--black pointer';
 
   function search(fuse, target) {
     return pluck('item', fuse.search(target));
@@ -32,16 +35,19 @@ document.addEventListener('DOMContentLoaded', function () {
     if (results.length === 0 && input.length === 0) {
       document.querySelector('#titleList').innerHTML = '';
     } else {
-      document.querySelector('#titleList').innerHTML = '<ul></ul>';
+      document.querySelector('#titleList').innerHTML = '<ul class="mw7 center db pl0"></ul>';
       results.map(function (result) {
         var link = document.createElement('A');
         link.href = '#';
         link.innerHTML = result.title;
+        link.className = 'dark-blue no-underline underline-hover'
         link.addEventListener('click', function (ev) {
           ev.preventDefault();
+          document.querySelector('#titleList').style.display = 'none';
           document.querySelector('#' + result.id).scrollIntoView({behavior: 'smooth'});
         });
         var item = document.createElement('LI');
+        item.className = 'mv1'
         item.insertAdjacentElement('beforeend', link);
         document.querySelector('#titleList ul').insertAdjacentElement('beforeend', item);
       });
@@ -62,6 +68,13 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     }
   });
+  // Show TitleList on focus:
+  document.querySelector('#searchBar').addEventListener('focus', function () {
+    if (document.querySelector('#titleList').style.display === 'none') {
+      document.querySelector('#titleList').style.display = 'block';
+    }
+  })
+
   // Filters as the user types in the search bar:
   document.querySelector('#searchBar').addEventListener('input', function (ev) {
     ev.preventDefault();
@@ -84,16 +97,17 @@ document.addEventListener('DOMContentLoaded', function () {
   categories.map(function (category) {
     var button = document.createElement('BUTTON');
     button.innerHTML = category;
+    button.className = BUTTON_CLASS;
     button.setAttribute('data-target', category);
     button.addEventListener('click', function (ev) {
       ev.preventDefault();
       var value = ev.target.getAttribute('data-target');
       var isActive = hasClass(ev.target, ACTIVE_CLASS);
-      toggleClass(ev.target, ACTIVE_CLASS);
-      nl2array(document.querySelector(ACTIVE_CLASS))
+      toggleClass(ev.target, ACTIVE_CLASS + ' ' + INACTIVE_CLASS);
+      nl2array(document.querySelectorAll('.' + ACTIVE_CLASS.replace(/ /g, '.')))
         .filter(function (element) {return element !== ev.target})
         .forEach(function (element) {
-          toggleClass(element, ACTIVE_CLASS);
+          toggleClass(element, ACTIVE_CLASS + ' ' + INACTIVE_CLASS);
         });
       document.querySelector('#questions').dispatchEvent(new CustomEvent('filter', {
         detail: {
@@ -103,6 +117,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }));
     });
     var item = document.createElement('LI');
+    item.className = 'dib'
     item.insertAdjacentElement('beforeend', button);
     document.querySelector('#categories ul').insertAdjacentElement('beforeend', item);
   });
